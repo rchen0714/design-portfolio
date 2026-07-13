767a4020b88d9c12e0a606591409b49198dd9d82
rchen0714 <125341510+rchen0714@users.noreply.github.com>
2026-07-13 16:40:36 +0800

    finish inputting placeholder text onto the homepage and fixing any bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,67 @@
 
+import TextType from "@/components/TextType";
 
 export default function Home() {
   return (
     <main>
       <section id="hero">
+        <div>
+          <div>
+            <h1>HI I&apos;M RUBY</h1>
+            <span className="hero-role-line">
+              <TextType
+                as="h1"
+                text={[
+                  "A PRODUCT DESIGNER",
+                  "A WEB DEVELOPER",
+                  "AN ILLUSTRATOR",
+                  "A CREATIVE TECHNOLOGIST",
+                ]}
+                typingSpeed={75}
+                deletingSpeed={40}
+                pauseDuration={1800}
+                showCursor={true}
+                cursorCharacter="|"
+                loop={true}
+              />
+            </span>
+          </div>
+
+          <div>
+            <p>Currently</p>
+            <p>Product Designer @ <a href="" target="_blank" rel="noopener noreferrer">Smata</a></p>
+            <p>Product Designer @ <a href="" target="_blank" rel="noopener noreferrer">Thrival</a></p>
+
+            <p>Currently</p>
+            <p>Founding Designer @ <a href="" target="_blank" rel="noopener noreferrer">TalentoraAI</a></p>
+          </div>
+
+          <div>
+            <p><a href="#selected-works" target="_blank" rel="noopener noreferrer">
+              View work
+            </a></p>
+            <p><a href="https://www.linkedin.com/in/rchen0714" target="_blank" rel="noopener noreferrer">
+              Linkedin
+            </a></p>
+          </div>
+
+        </div>
+
 
       </section>
 
       <section id="selected-works">
+        <h1>Selected Works</h1>
+        
+        <div>
+          <img></img>
+          <div>
+            <p>Talentora AI</p>
+            <p>Designed an AI-powered recruiting platform that streamlined candidate screening, interview scheduling, and recruiter workflows while improving the overall hiring experience.</p>
+            <button><a href="/work/talentora-ai" target="_blank" rel="noopener noreferrer">View Project</a></button>
+          </div>
+        </div>
+
         
       </section>
       
